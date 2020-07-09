16fbba9684c1fa78b3675360667606217706d059
Anna Józefowicz <anna.jozefowicz.86@gmail.com>
2020-07-10 00:42:13 +0200

    check local storage for user login credentials

--- client/src/views/RootPage.js
+++ client/src/views/RootPage.js
@@ -1,14 +1,54 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 import axios from "axios";
 import MainPage from "./MainPage";
 import UserPage from "./UserPage";
 import LoginPage from "./LoginPage";
+import setAuthToken from "../utils/setAuthToken";
 
 export default class RootPage extends Component {
   state = {
     notes: [],
     userNotes: [],
+    user: {},
+  };
+
+  componentDidMount() {
+    // Check for token to keep user logged in
+    if (localStorage.jwtToken) {
+      // Set auth token header auth
+      const token = localStorage.jwtToken;
+      setAuthToken(token);
+      // Decode token and get user info and exp
+      const decoded = jwt_decode(token);
+      console.log("decoded token");
+      console.log(decoded);
+      // Set user and isAuthenticated
+      this.setCurrentUser(decoded);
+      // Check for expired token
+      const currentTime = Date.now() / 1000; // to get in milliseconds
+
+      if (decoded.exp < currentTime) {
+        // token expired
+        // Logout user
+        localStorage.removeItem("jwtToken");
+        // Remove auth header for future requests
+        setAuthToken(false);
+        // Set current user to empty object {} which will set isAuthenticated to false
+        this.setCurrentUser({});
+        // Redirect to login
+        // window.location.href = "./login";
+      } else {
+        // Get user notes
+      }
+    } else {
+      // User is not authenticated. Redirect user to login page.
+    }
+  }
+
+  setCurrentUser = (user) => {
+    this.setState({ user });
   };
 
   getAllUsersNotes = () => {
@@ -44,11 +84,16 @@ export default class RootPage extends Component {
   };
 
   render() {
+    const { user } = this.state;
+
     return (
       <div>
         <Router>
           <div>
             <ul>
+              <h3>
+                {user.username ? <b>Hello {user.username}!</b> : "No user"}
+              </h3>
               <li>
                 <Link to="/">main page</Link>
               </li>
